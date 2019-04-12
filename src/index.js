82ee2d1831ac8229aee371481e18a030e0db6f71
Stephen Crowe <crowe.stephen.m@gmail.com>
2019-04-12 17:47:51 -0400

    we have some files in our project now

---
+++ src/index.js
@@ -0,0 +1,11 @@
+{
+    "name": "crowestephen.me",
+    "version": "1.0.0",
+    "main": "src/index.js",
+    "repository": "https://github.com/stephenmcrowe/crowestephen.me",
+    "author": "Stephen Crowe <crowe.stephen.m@gmail.com>",
+    "license": "MIT",
+    "scripts": {
+        "start": "node src/index.js"
+    }
+}
