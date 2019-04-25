dff08f06e075261939a9eccc0f7d14c1dd4c083c
Stephen Crowe <crowe.stephen.m@gmail.com>
2019-04-24 23:31:49 -0400

    router added to starterpack

--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    Switch, BrowserRouter as Router, Route, NavLink,
+} from 'react-router-dom';
 import './style.scss';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
+const About = (props) => {
+    return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+    return <div>Welcome</div>;
+};
+const Nav = (props) => {
+    return (
+        <nav>
+            <ul>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/test/id1">test id1</NavLink></li>
+                <li><NavLink to="/test/id2">test id2</NavLink></li>
+            </ul>
+        </nav>
+    );
+};
 
-        this.state = {
-        };
-    }
+const Test = (props) => {
+    return <div> ID: {props.match.params.id} </div>;
+};
 
-    render() {
-        return (
+const FallBack = (props) => {
+    return <div>URL Not Found</div>;
+};
+
+const App = (props) => {
+    return (
+        <Router>
             <div>
-                Hello!
+                <Nav />
+                <Switch>
+                    <Route exact path="/" component={Welcome} />
+                    <Route path="/about" component={About} />
+                    <Route exact path="/test/:id" component={Test} />
+                    <Route component={FallBack} />
+                </Switch>
             </div>
-        );
-    }
-}
+        </Router>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('main'));
