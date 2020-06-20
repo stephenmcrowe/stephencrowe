8f4ede7ca26e007665caf86e3e6d913c4c5e72e0
Stephen Crowe <crowe.stephen.m@gmail.com>
2020-06-20 02:26:51 -0700

    Working svgs with footer and portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,5 +1,6 @@
-/* eslint-disable max-len */
 import React, { Component } from 'react';
+
+import svgs from '../img';
 import content from '../assets/content.json';
 import '../styles/portfolio.scss';
 
@@ -7,10 +8,11 @@ import '../styles/portfolio.scss';
 class Portfolio extends Component {
   renderPortfolio = () => {
     return content.portfolio.map((data) => {
+      const Icon = svgs[data.logo];
       return (
         <div key={data.key} className="card">
           <div className="cardLogo">
-            <img src={data.logo} alt="" />
+            <Icon />
           </div>
           <div className="cardBody">
             {data.content}
---
+++ src/img/index.js
@@ -0,0 +1,41 @@
+/* eslint-disable camelcase */
+import { ReactComponent as dartmouth } from './dartmouth.svg';
+import { ReactComponent as grove } from './grove.svg';
+import { ReactComponent as minerva } from './minerva.svg';
+import { ReactComponent as nvidia } from './nvidia.svg';
+
+import { ReactComponent as chefskiss } from './chefskiss.svg';
+import { ReactComponent as cpu } from './cpu.svg';
+import { ReactComponent as gut_busters } from './gut_busters.svg';
+import { ReactComponent as hackdartmouth_logo } from './hackdartmouth_logo.svg';
+import { ReactComponent as handout } from './handout.svg';
+import { ReactComponent as monitor_mac } from './monitor_mac.svg';
+import { ReactComponent as pantri } from './pantri.svg';
+import { ReactComponent as search } from './search.svg';
+
+import { ReactComponent as facebook } from './facebook.svg';
+import { ReactComponent as github } from './github.svg';
+import { ReactComponent as linkedin } from './linkedin.svg';
+
+
+const componentsMap = {
+  dartmouth,
+  grove,
+  minerva,
+  nvidia,
+
+  chefskiss,
+  cpu,
+  gut_busters,
+  hackdartmouth_logo,
+  handout,
+  monitor_mac,
+  pantri,
+  search,
+
+  facebook,
+  github,
+  linkedin,
+};
+
+export default componentsMap;
